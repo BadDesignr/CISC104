426568d8b3198e08bed92d04f26381d59e29ddfe
BadDesignr <113140367+BadDesignr@users.noreply.github.com>
2022-10-27 15:27:01 -0400

    Project
    project actually added this time

---
+++ My project/Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    //movement speed in units per second
+    private float movementSpeed = 5f;
+
+    void Update()
+    {
+        //get the Input from Horizontal axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //get the Input from Vertical axis
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //update the position
+        transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, 0, verticalInput * movementSpeed * Time.deltaTime);
+
+        //output to log the position change
+        Debug.Log(transform.position);
+        }
+    }
